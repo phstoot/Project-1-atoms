d5568d722c083315f6a6759d9efab38e72184384
NilsThiessen <Nils.Thiessen@web.de>
2026-02-14 12:45:59 +0100

    extrapolating minimal image convention to general case. starting plotting process

--- main.py
+++ main.py
@@ -30,6 +30,25 @@ def min_vector(part1, part2):
     min_vec = np.mod(vec + [0.5*L, 0.5*L], [L,L]) - [0.5*L, 0.5*L]
     return min_vec
 
+def periodic_boundaries(pos):
+    '''Causes the particles to stay in the box defined by (L,L).'''
+    for i in range(N):
+        pos[i,0] %= L
+        pos[i,1] %= L
+        pos[pos < 0] += L
+
+
+
+## Define particles (init properties)
+# Probably start with 3-4 particles first
+
+# shall we create a particle class?
+class Particle:
+    def __init__(self, position, velocity, mass):
+        self.position = position
+        self.velocity = velocity
+        self.mass = mass
+
 # Initial Position
 pos = np.random.uniform(0,L,size=(N,2))
 vel = np.random.uniform(0,L/5, size=(N,2))# np.zeros((N,2))
@@ -109,8 +128,7 @@ def simulate():
     vel[:,1] += 1 / mass * Summed_Force[:,1] * h
     
     #Periodic boundary conditions (Needs to be adjusted, so that if a particle with pos > 2L is floored to its remainder between (0,L))
-    pos[pos >L] -= L# np.mod(pos, [L,L]) [pos>L]
-    pos[pos<0] += L
+    periodic_boundaries(pos)
     
     
     print("New Positions: "+ str(pos))
@@ -138,26 +156,41 @@ print(str(All_vel))
 
 
 
+## Regular Evolution Plot
+def static_plot(positions, velocities):
+    '''
+    Plots the motion of the particles on one plot.
+    
+    '''   
+    
+    
+    # Define colours for plot
+    colors = []
+    for i in range(N): # from some stackexchange post
+        colors.append('#%06X' % randint(0, 0xFFFFFF))
+        
+    plt.figure(figsize=(8,8))    
+    
+    for i in range(N):
+    plt.scatter(positions[i,0,:], positions[i,1,:], c= colors[i], marker = 'o', linewidths= 1)
 
-## Define colours for plot
-colors = []
 
-for i in range(N): # from some stackexchange post
-    colors.append('#%06X' % randint(0, 0xFFFFFF))
-
-
-plt.figure(figsize=(8,8))
+    plt.title(r"Simulation for " + str(N) + " different particles")
+    plt.xlabel(r"x")
+    plt.ylabel(r"y")
+    plt.grid(True, linestyle='--', alpha=0.5)
+    plt.show()
 
 
-for i in range(N):
-    plt.scatter(All_pos[i,0,:], All_pos[i,1,:], c= colors[i], marker = 'o', linewidths= 1)
+# ## Active animation
+# def animated_plot():
+#     .
+
+static_plot(All_pos, All_vel)
 
 
-plt.title(r"Simulation for " + str(N) + " different particles")
-plt.xlabel(r"x")
-plt.ylabel(r"y")
-plt.grid(True, linestyle='--', alpha=0.5)
-plt.show()
+
+
 
 ## Things still to do: 
 # Make a more understandable plot (especially when particles go haywire, their evolution cannot be captured in the plot)
