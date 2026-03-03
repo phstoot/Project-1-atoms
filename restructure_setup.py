a19661f1ef615ab6fb9790247e3d3c0c2327169b
phstoot <stoot.philip@gmail.com>
2026-03-03 23:15:53 +0100

    Proposed setup for a Simulation class

---
+++ restructure_setup.py
@@ -0,0 +1,39 @@
+class Simulation:
+    """
+    Docstring here
+    
+    """
+
+    def __init__(self, density, temp, N_particles=108, L=10, dim=3):
+        self.density = density
+        #etc
+        pass
+
+    def __repr__(self):
+        # write a formal representation for this class
+        pass
+
+    def __str__(self):
+        # write an informal string representation for this class
+        pass
+
+    def equilibrate(self):
+        # see lecture 4 and coding guidelines
+        pass
+
+    def simulate(self, algorithm, time):
+        # see lecture 4
+        # time how long to simulate?
+        # algorithm: euler or verlet
+
+        #i'd say: based on chosen algorithm, import correct function
+        # from other file
+        pass
+
+    def quickshow(self):
+        # just an idea
+        pass
+
+    def save(self):
+        # and specify in what form
+        pass
